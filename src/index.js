f9fc394bacdf5f0976674d942542d2cc361611c8
monika12b <monika12b@gmail.com>
2024-06-28 16:57:53 -0700

    react package update, adding react-router-dom, file name changes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter >
   </React.StrictMode>,
   document.getElementById("root")
 );
