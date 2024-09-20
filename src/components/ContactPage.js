707525ee4c5d517153b93c99ec1221ea5ee4ba05
monika12b <monika12b@gmail.com>
2024-09-20 16:38:00 -0700

    adding opening texts to contact page

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -5,6 +5,15 @@ import "../styled-components/ContactPage.css"
 const ContactPage = () => {
   return (
     <div id="contact" className="contact-page">
+      <div className="contact-page-text-wrapper">
+        <p>
+          Got an exciting <span className="highlight-text">job opportunity</span>? I’d love to hear about it!
+        </p>
+        <p>
+          <span className="highlight-text">Let’s say "hi"</span> and get to know each other, <span className="highlight-text-orange">or</span> feel free to <span className="highlight-text">share your thoughts on my portfolio</span>!
+        </p>
+      </div>
+
       <GoogleForm />
     </div>
   )
